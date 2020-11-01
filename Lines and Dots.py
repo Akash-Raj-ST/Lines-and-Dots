e9771db52e86e810a220d6416501dc9ffec5d8c9
Akash-Raj-ST <65654411+Akash-Raj-ST@users.noreply.github.com>
2020-11-01 21:55:44 +0530

    Add files via upload
    By using this python program, the number of minimum dots required to connect the given number of lines can be obtained.
    Input:
     ---Number of lines.
    Output:
    ---Number of minimum dots required to connect the given number of lines

---
+++ Lines and Dots.py
@@ -0,0 +1,26 @@
+n=int(input("No of Lines:"))
+fact=1
+fact1=[1]
+fact2=[0,1,1]
+for i in range(1,100):
+    t=0
+    fact=fact*i
+    fact1.append(fact)
+    fact2.append(fact*2)
+    if fact1[-1]/fact2[-3]==n:
+        if n>=3:
+            l=len(fact1)-1
+            break
+        elif n==1:
+            l=len(fact1)
+            break     
+        else:
+            t=1
+            break
+    else:
+        t=1
+if t==1:  
+    print("Maximum no of lines can be drawn using these points")
+if fact1[-1]/fact2[-3]==n and t!=1:
+    print("Dots Required: ",l)
+        
